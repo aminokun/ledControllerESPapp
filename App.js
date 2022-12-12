0043f57181ae54e8fc013f7fe404dad3f8185697
amine <amine.dbh99@gmail.com>
2022-12-12 10:27:13 +0100

    end

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react';
 
 function App(){
@@ -6,9 +6,9 @@ function App(){
 
   const toggleLedOn = async () => {
     try {
-      const response = await fetch('http://192.168.178.74/led/on');
+      const response = await fetch('http://192.168.238.82/led/on');
       const result = await response.text();
-      if (result === 'LED turned on' || result === 'LED turned off') {
+      if (result === 'LED turned on' || result === 'LED turned on') {
         setIsLedOn(!isLedOn);
       } else {
         // Handle error
@@ -19,7 +19,7 @@ function App(){
   };
   const toggleLedOff = async () => {
     try {
-      const response = await fetch('http://192.168.178.74/led/off');
+      const response = await fetch('http://192.168.238.82/led/off');
       const result = await response.text();
       if (result === 'LED turned on' || result === 'LED turned off') {
         setIsLedOn(!isLedOn);
@@ -33,9 +33,21 @@ function App(){
   
   return (
     <View style={styles.container}> 
-      <Text>LED is {isLedOn ? 'on' : 'off'}</Text>
-      <Button title="Toggle LED on" onPress={toggleLedOn} />
-      <Button title="Toggle LED off" onPress={toggleLedOff} />
+      <Text style={styles.title}>
+        Turn LED on or off {isLedOn ? "On" : "Off"}
+      </Text>
+        <View style={styles.buttons}>
+        <TouchableOpacity style={styles.button} title="Toggle LED on" onPress={toggleLedOn} >
+            <Text style={styles.buttonText}>
+              ON
+            </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} title="Toggle LED off" onPress={toggleLedOff} >
+            <Text style={styles.buttonText}>
+              OFF
+            </Text>
+        </TouchableOpacity>
+        </View>
     </View>
   );
 };
@@ -46,6 +58,33 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 22,
+  },
+    button: {
+    marginTop: "20%",
+    marginHorizontal: "5%",
+    height: 50,
+    width: 150,
+    backgroundColor: '#28b4ee',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+  },
+    buttonText: {
+    fontWeight: 'regular',
+    fontSize: 22,
+    color: '#fff',
+  },  
+    title: {
+    fontStyle: "bold",
+    fontSize: 22,
+  },
+    buttons: {
+    marginTop: 200,
+    flexDirection: "row",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
